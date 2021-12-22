e7deed2beb22bd460a33d02b7c646040780742d3
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2021-12-22 11:44:30 -0500

    clarify

--- ana_tudor/js/update-custom-props.js
+++ ana_tudor/js/update-custom-props.js
@@ -3,6 +3,7 @@
 // (For an actual use case, they'd be set higher up in the DOM so that
 // they're also inherited by the elements whose styles that depend on them.)
 addEventListener('input', e => {
-  const _t = e.target
-  _t.parentNode.style.setProperty(`--${_t.id}`, +_t.value)
+  let range = e.target
+  let wrapper = range.parentNode
+  wrapper.style.setProperty(`--${range.id}`, +range.value)
 }, false)
