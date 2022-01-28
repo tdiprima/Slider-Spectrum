24b2e9ea8a00300bdd9169118474686777c3cb27
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2022-01-28 14:00:19 -0500

    td

--- ana_tudor/js/e.js
+++ ana_tudor/js/e.js
@@ -1,7 +1,3 @@
-/* As for your JavaScript, you could abstract away your element creation with
-a function like the following. This way, you don't have to repeat element creation
-scripts everywhere and you can easily describe dynamic HTML in a nested manner,
-like HTML. */
 // https://codereview.stackexchange.com/questions/213431/adding-li-items-dynamically-to-a-to-do-list
 const e = (name, properties = {}, children = []) => {
   // Create the element
